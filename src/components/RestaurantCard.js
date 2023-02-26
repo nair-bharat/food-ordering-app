025f2adba95b981290b6f0fbfa5751444d691250
nair-bharat <bharatmnair246@gmail.com>
2023-02-26 15:07:28 +0530

    fixing the rating

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -1,6 +1,7 @@
 import { IMG_CDN_URL } from "../utils/constants";
 import veg_icon from "../assets/images/veg-icon.png";
 import non_veg_icon from "../assets/images/non-veg-icon.png";
+import star_rating from "../assets/images/star-rating.png";
 
 const RestaurantCard = ({
   cloudinaryImageId,
@@ -28,21 +29,23 @@ const RestaurantCard = ({
       </div>
 
       <div className="text-xs lg:text-sm">{cuisines?.join(", ")}</div>
-      <div className="lg:flex text-xs font-medium lg:py-3 flex flex-col ">
-        <div className="bg-green-500 text-white lg:flex flex">
-          <span className="py-1 pl-1">
-            <svg
-              aria-hidden="true"
-              className="w-3 h-4  text-xs text-white"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
-            </svg>
-          </span>
-          <span className="p-1 pr-1.5">{avgRating}</span>
-        </div>
+      <div className="lg:flex text-xs font-medium lg:py-3">
+        {parseFloat(avgRating) >= 4 ? (
+          <div className="text-white lg:flex flex bg-green-500">
+            <span className="pt-1.5 pl-1">
+              <img src={star_rating} alt="star" className="w-3 h-3"></img>
+            </span>
+            <span className="p-1 pr-1.5">{avgRating}</span>
+          </div>
+        ) : (
+          <div className="text-white lg:flex flex bg-orange-500">
+            <span className="pt-1.5 pl-1">
+              <img src={star_rating} alt="star" className="w-3 h-3"></img>
+            </span>
+            <span className="p-1 pr-1.5">{avgRating}</span>
+          </div>
+        )}
+
         <div className="text-xs px-2 py-1 font-medium text-gray-600">
           {deliveryTime} MINS
         </div>
--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -6,6 +6,7 @@ import { MENU_API } from "../utils/constants";
 import { IMG_CDN_URL } from "../utils/constants";
 import ShimmerMenu from "./ShimmerMenu";
 import { convertPrice } from "../utils/convertPrice";
+import star_rating from "../assets/images/star-rating.png";
 
 const RestaurantMenu = () => {
   const { id } = useParams();
@@ -43,27 +44,23 @@ const RestaurantMenu = () => {
           src={IMG_CDN_URL + restaurant?.cloudinaryImageId}
         ></img>
         <div className="md:p-5 p-2">
-          <h2 className="font-bold md:text-2xl md:px-5 text-lg">{restaurant?.name}</h2>
+          <h2 className="font-bold md:text-2xl md:px-5 text-lg">
+            {restaurant?.name}
+          </h2>
           <p className="font-extralight md:text-md md:px-5 text-sm">
             {restaurant?.cuisines?.join(", ")}
           </p>
           <div className="flex md:font-bold md:text-sm text-xs">
             <div className="flex md:p-5 pt-1">
-              <svg
-                stroke="currentColor"
-                fill="currentColor"
-                strokeWidth="0"
-                viewBox="0 0 1024 1024"
-                height="1.25rem"
-                width="1rem"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path d="M908.1 353.1l-253.9-36.9L540.7 86.1c-3.1-6.3-8.2-11.4-14.5-14.5-15.8-7.8-35-1.3-42.9 14.5L369.8 316.2l-253.9 36.9c-7 1-13.4 4.3-18.3 9.3a32.05 32.05 0 0 0 .6 45.3l183.7 179.1-43.4 252.9a31.95 31.95 0 0 0 46.4 33.7L512 754l227.1 119.4c6.2 3.3 13.4 4.4 20.3 3.2 17.4-3 29.1-19.5 26.1-36.9l-43.4-252.9 183.7-179.1c5-4.9 8.3-11.3 9.3-18.3 2.7-17.5-9.5-33.7-27-36.3z"></path>
-              </svg>
-              <span>{restaurant?.avgRatingString}</span>
+              <span className="w-4 h-4 pt-0.5 mx-0.5">
+                <img src={star_rating} alt="star"></img>
+              </span>
+              <span>{restaurant?.avgRating}</span>
             </div>
             <div className="md:p-5 p-1">|</div>
-            <div className="md:p-5 p-1">{restaurant?.sla?.deliveryTime} MINS</div>
+            <div className="md:p-5 p-1">
+              {restaurant?.sla?.deliveryTime} MINS
+            </div>
             <div className="md:p-5 p-1">|</div>
             <div className="md:p-5 p-1">{restaurant?.costForTwoMsg}</div>
           </div>
@@ -74,10 +71,17 @@ const RestaurantMenu = () => {
         {Object.values(restaurant?.menu?.items)
           ?.slice(0, 20)
           ?.map((item) => (
-            <div className="md:p-5 p-2 md:m-5 m-2 mt-5 border border-solid flex md:w-[650px] w-[350px] justify-between" key={item.id}>
+            <div
+              className="md:p-5 p-2 md:m-5 m-2 mt-5 border border-solid flex md:w-[650px] w-[350px] justify-between"
+              key={item.id}
+            >
               <div className="p-4">
-                <div className="font-bold md:text-md text-sm p-1 ">{item?.name}</div>
-                <div className="md:text-sm text-xs p-1">₹ {convertPrice(item?.price)}</div>
+                <div className="font-bold md:text-md text-sm p-1 ">
+                  {item?.name}
+                </div>
+                <div className="md:text-sm text-xs p-1">
+                  ₹ {convertPrice(item?.price)}
+                </div>
                 <div className="font-extralight md:text-sm text-xs p-1">
                   {item?.category}
                 </div>
